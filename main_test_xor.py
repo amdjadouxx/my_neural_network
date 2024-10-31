8fd54b20f3085a5d6aa8333f4e5b55c3344326df
amdjadouxx <amdjad.ahmod-ali@epitech.eu>
2024-10-31 16:05:00 +0400

    [IMPROVE] xor clean

--- main_test_xor.py
+++ main_test_xor.py
@@ -8,18 +8,16 @@ from Network import Network
 x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
 y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
 
-# net = Network(loss=mse, loss_prime=mse_prime)
-# net.add(FCLayer(2, 3))
-# net.add(ActivationLayer(tanh, tanh_prime))
-# net.add(FCLayer(3, 1))
-# net.add(ActivationLayer(sigmoid, sigmoid_prime))
+net = Network(loss=mse, loss_prime=mse_prime)
+net.add(FCLayer(2, 3))
+net.add(ActivationLayer(tanh, tanh_prime))
+net.add(FCLayer(3, 1))
+net.add(ActivationLayer(sigmoid, sigmoid_prime))
 
-# net.fit(x_train, y_train, epochs=2000, learning_rate=0.1, silent=True)
-net = Network()
-net.load("xor_model")
+net.fit(x_train, y_train, epochs=2000, learning_rate=0.1, silent=True)
 net.summary()
-# net.disp_loss_graph()
-# net.disp_loss_accuracy_graph()
+net.disp_loss_graph()
+net.disp_loss_accuracy_graph()
 
 out = net.predict(x_train)
 for vidx in range(len(out)):
@@ -28,6 +26,3 @@ for vidx in range(len(out)):
     else:
         out[vidx] = 0
 print(out)
-
-if net.evaluate(x_train, y_train) == 1:
-    net.save("xor_model")
