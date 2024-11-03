9c49392b8c7968ff2e5a72a5177ae17c67a3ed97
amdjadouxx <amdjad.ahmod-ali@epitech.eu>
2024-11-03 23:18:40 +0400

    [CLEAN] disable save and load for the moment => next step optimizer

--- main_test_mnist.py
+++ main_test_mnist.py
@@ -23,39 +23,21 @@ x_test /= 255
 y_test = to_categorical(y_test)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main_test_mnist.py [train/test]")
-        sys.exit(84)
+    net = Network(loss=mse, loss_prime=mse_prime)
+    net.add(FCLayer(28*28, 100))
+    net.add(ActivationLayer(tanh, tanh_prime))
+    net.add(FCLayer(100, 50))
+    net.add(ActivationLayer(tanh, tanh_prime))
+    net.add(FCLayer(50, 10))
+    net.add(ActivationLayer(tanh, tanh_prime))
+    net.fit(x_train[0:1000], y_train[0:1000], epochs=100, learning_rate=0.1, silent=False)
 
-    if sys.argv[1] == "train":
-        net = Network(loss=mse, loss_prime=mse_prime)
-        net.add(FCLayer(28*28, 100))
-        net.add(ActivationLayer(tanh, tanh_prime))
-        net.add(FCLayer(100, 50))
-        net.add(ActivationLayer(tanh, tanh_prime))
-        net.add(FCLayer(50, 10))
-        net.add(ActivationLayer(tanh, tanh_prime))
-        net.fit(x_train[0:1000], y_train[0:1000], epochs=100, learning_rate=0.1, silent=False)
+    net.summary()
 
-        net.summary()
+    acc = net.evaluate(x_train[0:1000], y_train[0:1000], silent=False)
 
-        acc = net.evaluate(x_train[0:1000], y_train[0:1000], silent=False)
-
-        if acc > 0.9:
-            net.save("mnist_model")
-        net.disp_loss_accuracy_graph()
-        net.show()
-
-    elif sys.argv[1] == "test":
-        net = Network()
-        net.load("mnist_model")
-        net.summary()
-
-        net.evaluate(x_train[0:1000], y_train[0:1000], silent=False)
-
-    else:
-        print("Invalid argument")
-        sys.exit(84)
+    net.disp_loss_accuracy_graph()
+    net.show()
 
     out = net.predict(x_test[0:3])
     for i in range(3):
